39e6662177c5a81d4c55866a0e972989aa30f750
IgoRenatoo <igor.renato.oliveira@gmail.com>
2025-03-26 13:21:36 -0300

    feat: add exercise 05 - introduction

--- 01 - Introduction/src/Exercise05.java
+++ 01 - Introduction/src/Exercise05.java
@@ -1,3 +1,27 @@
+import java.util.Scanner;
+
+/*
+ * O programa verifica se um número inserido é primo.
+ */
+
 public class Exercise05 {
-  
+  public static void main(String[] args) {
+    Scanner number = new Scanner(System.in);
+
+    System.out.println("Informe um número para verificar se ele é primo: ");
+    int num = number.nextInt();
+    int isCousin = 0;
+
+    for (int i = 1; i <= num; i++) {
+      if (num%i == 0) {
+        isCousin++;
+      }
+    }
+    if (isCousin == 2) {
+      System.out.println("O número " + num + " é um número primo!");
+    } else {
+      System.out.println("O número " + num + " não é um número primo!");
+    }
+    number.close();
+  }
 }
