4ed1974cfc14bb0d94208c83ef6edd1c8e066837
IgoRenatoo <igor.renato.oliveira@gmail.com>
2025-03-26 10:01:17 -0300

    feat: add exercise 04 - introduction

--- 01 - Introduction/src/Exercise04.java
+++ 01 - Introduction/src/Exercise04.java
@@ -1,3 +1,22 @@
+import java.util.Scanner;
+
+/*
+ * Recebe uma palavra e exibe-a invertida.
+ */
+
 public class Exercise04 {
-  
+  public static void main(String[] args) {
+    Scanner word = new Scanner(System.in);
+
+    System.out.println("Informe uma palavra com mais de 2 caracteres: ");
+    String newWord = word.nextLine();
+
+    if (newWord.length() <= 2){
+      System.out.println("Quantidade de caracteres insuficiente...");
+    } else {
+      String inverseWord = new StringBuilder(newWord).reverse().toString();
+      System.out.println(inverseWord);
+    }
+    word.close();
+  }
 }
