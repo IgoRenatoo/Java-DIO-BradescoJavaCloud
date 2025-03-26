a141fdc20ac861b0ebf2379979cdb582fef4fe02
IgoRenatoo <igor.renato.oliveira@gmail.com>
2025-03-26 14:13:53 -0300

    feat: add exercise 07 - introduction

--- 01 - Introduction/src/Exercise07.java
+++ 01 - Introduction/src/Exercise07.java
@@ -1,3 +1,29 @@
+import java.util.Scanner;
+
+/*
+ * O programa simula um caixa eletrônico que informa a quantidade de cédulas necessárias para um determinado valor de saque.
+ */
+
 public class Exercise07 {
-  
+  public static void main(String[] Args) {
+    Scanner value = new Scanner(System.in);
+
+    System.out.println("Informe o Valor a ser sacado: ");
+    int amountMoney = value.nextInt();
+    int count = 0;
+    int result = 0;
+    int[] ballot = {100, 50, 20, 10, 5, 2, 1};
+
+    do {
+      if (amountMoney >= ballot[count]){
+        result += amountMoney/ballot[count];
+        amountMoney = amountMoney%ballot[count];;
+        count++;
+      } else {
+        count++;
+      }
+    } while (amountMoney != 0);
+    System.out.println("A quantidade de notas para o valor informado é: " + result);
+    value.close();
+  }
 }
