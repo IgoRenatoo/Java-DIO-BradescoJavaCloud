4bded015795956823a3edc1613fd6ce093096a80
IgoRenatoo <igor.renato.oliveira@gmail.com>
2025-03-30 18:12:06 -0300

    feat: add exercise 04 - poo

--- 02 - POO/Exercise03/datauser/Address.java
+++ 02 - POO/Exercise03/datauser/Address.java
@@ -8,27 +8,32 @@
 
   ❓ *Perguntas Técnicas*
     1. Qual é a diferença entre associação e composição em POO?
+      R ~> Associação: Uma classe usa outra, mas ambas podem existir separadamente.
+           Exemplo: Pessoa tem um Endereco, mas pode mudar ou existir sem ele.
     2. Por que o atributo endereco não pode ser do tipo primitivo?
+      R ~> Porque ele é um Objeto contendo 2 propriedades.
     3. Como a criação de objetos pode influenciar o consumo de memória?
+      R ~> Associação: Objetos ocupam memória Heap. Muitos objetos podem causar alto consumo, vazamento e queda de desempenho.
+           O Garbage Collector ajuda, mas boas práticas evitam desperdício.
 */
-package DataUser;
+package datauser;
 
 public class Address {
   private String road;
   private String city;
 
-  void addAddress(String addRoad, String addCity){
+  void addNewAddress(String addRoad, String addCity) {
     this.road = addRoad;
     this.city = addCity;
   }
 
-  void changeRoad(String newRoad){
+  void changeRoad(String newRoad) {
     this.road = newRoad;
   }
-  void changeCity(String newCity){
+  void changeCity(String newCity) {
     this.city = newCity;
   }
-  void displayData() {
-    System.out.println("Sua rua é: " + this.road + ", sua cidade é: " + this.city);
+  String displayData() {
+    return "Sua rua é: " + this.road + ", sua cidade é: " + this.city;
   }
 }
--- 02 - POO/Exercise03/datauser/Person.java
+++ 02 - POO/Exercise03/datauser/Person.java
@@ -8,10 +8,31 @@
 
   ❓ *Perguntas Técnicas*
     1. Qual é a diferença entre associação e composição em POO?
+      R ~> Associação: Uma classe usa outra, mas ambas podem existir separadamente.
+           Exemplo: Pessoa tem um Endereco, mas pode mudar ou existir sem ele.
     2. Por que o atributo endereco não pode ser do tipo primitivo?
+      R ~> Porque ele é um Objeto contendo 2 propriedades.
     3. Como a criação de objetos pode influenciar o consumo de memória?
+      R ~> Associação: Objetos ocupam memória Heap. Muitos objetos podem causar alto consumo, vazamento e queda de desempenho.
+           O Garbage Collector ajuda, mas boas práticas evitam desperdício.
 */
-package DataUser;
-public class Person {
+package datauser;
 
+public class Person {
+  private String name;
+  private Address address;
+
+  void addDataUser(String addName, Address newAddress){
+    this.name = addName;
+    this.address = newAddress;
+  }
+
+  public static void main(String[] args) {
+    Address newAddress = new Address();
+    Person newUser = new Person();
+
+    newAddress.addNewAddress("Rua Dulci", "Vila Velha!");
+    newUser.addDataUser("Igor", newAddress);
+    System.out.println("Seu nome é: " + newUser.name + ", " + newUser.address.displayData());
+  }
 }
