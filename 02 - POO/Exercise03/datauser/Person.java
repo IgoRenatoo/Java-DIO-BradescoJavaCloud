daf639feded3d6d07d4932a14fe6c018a14f89eb
IgoRenatoo <igor.renato.oliveira@gmail.com>
2025-03-30 17:37:22 -0300

    feat: add exercise 03 - poo

---
+++ 02 - POO/Exercise03/datauser/Person.java
@@ -0,0 +1,17 @@
+/*
+  *Exercício 3: Criando uma Classe com Relacionamento*
+    Crie duas classes: Pessoa e Endereco.
+    - A classe Endereco deve ter os atributos rua e cidade.
+    - A classe Pessoa deve ter os atributos nome e endereco (do tipo Endereco).
+    - Adicione métodos para definir e obter os valores.
+    - No méthodo main, instancie um objeto Pessoa e associe um endereço a ele.
+
+  ❓ *Perguntas Técnicas*
+    1. Qual é a diferença entre associação e composição em POO?
+    2. Por que o atributo endereco não pode ser do tipo primitivo?
+    3. Como a criação de objetos pode influenciar o consumo de memória?
+*/
+package DataUser;
+public class Person {
+
+}
